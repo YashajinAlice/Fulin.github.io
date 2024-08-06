e0149e17dcffd82f1faf9cde9f9db44a39a2c166
Alice <138005616+YashajinAlice@users.noreply.github.com>
2024-08-06 21:41:12 +0800

    Site updated: 2024-08-06 21:41:12

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722951451799";
+const VERSION = "1722951671658";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
